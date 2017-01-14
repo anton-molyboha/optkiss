51dc84321a78e3252bf3e09447264d972425c528
anton <anton.molyboha@gmail.com>
2017-01-14 10:46:06 -0600

    Torczon algorithm test: add Rosenbrock function

--- test/optkiss/torczon.py
+++ test/optkiss/torczon.py
@@ -1,5 +1,7 @@
 import numpy as np
 import optkiss.torczon as ot
+from scipy.optimize import rosen
+
 
 def norm(n):
     def objf(x, y):
@@ -11,6 +13,15 @@ def norm(n):
     #print(x)
     return np.linalg.norm(x) < 1e-3
 
+
+def rosenbrock(n):
+    def objf(x, y):
+        return y - rosen(x)
+    x = ot.torczon_implicit(objf, np.zeros(n), 0.0)
+    print(x)
+    return np.linalg.norm(x - np.ones(n)) < 1e-3
+
 if __name__ == "__main__":
     print("Testing minimization of ||x|| in R^3: " + str(norm(3)))
     print("Testing minimization of ||x|| in R^{300}: " + str(norm(300)))
+    print("Testing minimization of Rosenbrock in R^2: " + str(rosenbrock(2)))
